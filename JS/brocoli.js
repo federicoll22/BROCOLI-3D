9f5527557b38f2f9044f019e0dcd357b516895ab
federicoll22 <llanos.fede@gmail.com>
2021-07-21 07:07:53 -0300

    ultimo commit

--- JS/brocoli.js
+++ JS/brocoli.js
@@ -1,19 +1,38 @@
-    var regresar = $("#go-top");
+var regresar = $("#go-top");
 
-    $(window).scroll(function (){
-        if($(window).scrollTop() > 350){
-            regresar.addClass("visible");
-        }else{
-            regresar.removeClass("visible")
-        }
-    });
+$(window).scroll(function (){
+    if($(window).scrollTop() > 350){
+        regresar.addClass("visible");
+    }else{
+        regresar.removeClass("visible")
+    }
+});
 
-    regresar.on("click", function (evento) {
-        evento.preventDefault();
-        $("html, body").animate ({scrollTop: 0}, 300);
-    })
+regresar.on("click", function (evento) {
+    evento.preventDefault();
+    $("html, body").animate ({scrollTop: 0}, 300);
+})
 
-    
+const addtoshop = document.querySelectorAll('.btn-outline-secondary');
+addtoshop.forEach((addTocardBtton) => {
+addTocardBtton.addEventListener('click', addToCartClicked);
+});
+
+function addToCartClicked(event) {
+    const button = event.target;
+    const item = button.closest('.card-body');
+      
+    const itemTitle = card-body.querySelector('.item-title').textContent;
+    const itemPrice = card-body.querySelector('.item-price').textContent;
+    const itemImage = card-body.querySelector('.item-image').src;
+      
+    addItemToShoppingCart(itemTitle, itemPrice, itemImage);
+}
+
+function addItemToShopingCard(itemTitle,itemPrice,itemImage){
+    console.log(itemTitle,itemPrice,itemImage);
+}
+
     
 
 
